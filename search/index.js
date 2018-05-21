1870ac4e2f5ea7bade6631d4060a8d7c0f185e6f
Anton Shishenya <anton.shishenya@gmail.com>
2018-05-21 17:46:12 +0300

    Add regExp for email

--- search/index.js
+++ search/index.js
@@ -67,9 +67,22 @@ class Searcher {
 
         let query = {query: {}};
 
+        let isEmail = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+
         if (options.fields && options.q) {
+            let reg = /[^\w\s\.]/gi;
+            let checkNumberEmail = false;
+            if (isEmail.test(options.q)) {
+                let emailArr = options.q.split(".");
+                let lastSymbol = emailArr[emailArr.length - 2];
+                checkNumberEmail = Number.isInteger(parseInt(lastSymbol[lastSymbol.length - 1]));
+            }
+
+            if (checkNumberEmail) {
+                reg = /[^\w\s]/gi;
+            }
             query.query.bool = query.query.bool || {};
-            options.q = options.q.replace(/[^\w\s\.]/gi, " AND ");
+            options.q = options.q.replace(reg, " AND ");
             query.query.bool.must = {
                 query_string: {
                     query: `*${options.q}*`,
