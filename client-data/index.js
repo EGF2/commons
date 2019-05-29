0d05390767440b376507b5af1ef9460ed8f383d0
win-dox <schamraev.d@yandex.ru>
2019-05-29 11:09:30 +0300

    continue -> return

--- client-data/index.js
+++ client-data/index.js
@@ -303,14 +303,8 @@ function newClient(url) {
                             }
                             let promise;
                             if (field in obj && field in objCfg.fields) {
-                                if (!obj[field]) continue;
-                                try {
-                                    promise = this.getObject(obj[field], options);
-
-                                } catch (e) {
-                                    console.log(e, obj[field], field, obj);
-                                    throw e
-                                }
+                                if (!obj[field]) return;
+                                promise = this.getObject(obj[field], options);
                             } else if (objCfg.edges && field in objCfg.edges) {
                                 promise = this.getEdges(obj.id, field, options);
                             }
