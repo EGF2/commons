25d584df8dcddd9a1e653f4b4221221df28a454e
win-dox <schamraev.d@yandex.ru>
2019-07-12 11:13:19 +0300

    Add V2

---
+++ client-dataV2/index.js
@@ -0,0 +1,326 @@
+"use strict";
+
+const restify = require("restify");
+
+function newClient(url) {module.exports = newClient;
+
+    let client = restify.createJsonClient({
+        url,
+        version: "*"
+    });
+    // Time in ms
+    const startTimeout = 5;
+    const deltaInterval = 20;
+    const maxTimeout = 3500;
+
+    let request = (method, params) => {
+        return new Promise((resolve, reject) => {
+            let callback = (err, req, res, obj) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(obj);
+            };
+            if (method === "GET") {
+                client.get(params.options, callback);
+            } else if (method === "POST") {
+                client.post(params.options, params.body, callback);
+            } else if (method === "PUT") {
+                client.put(params.options, params.body, callback);
+            } else if (method === "PATCH") {
+                client.patch(params.options, params.body, callback);
+            } else if (method === "DELETE") {
+                client.del(params.options, callback);
+            }
+        });
+    };
+
+    const timeout = async ms => {
+        return new Promise(res => setTimeout(res, ms));
+    };
+
+    const handle = async (method, path, body, author, suppressEvent) => {
+        let options = { path };
+        if (author) {
+            options.headers = {
+                "Author": author
+            };
+        }
+        if (suppressEvent) {
+            options.headers = {
+                suppressEvent: true
+            }
+        }
+        let err;
+        let waitTime = 0;
+        const objErr = {};
+        for (let i = startTimeout; waitTime <= maxTimeout; i += deltaInterval) {
+            try {
+                const res = await request(method, { options, body });
+                return res;
+            } catch (e) {
+                err = e;
+                if (!objErr.err) objErr.err = { err: e, message: e.message, code: e.code }
+                if (!e.message.includes("Gateway")) break;
+                await timeout(i);
+                waitTime += i;
+                continue;
+            }
+        }
+        throw err;
+    };
+
+    // keep graph config
+    let graphConfig;
+
+    // keep object code to object type map
+    let codeToObjectType;
+
+    return {
+        /**
+         * Get graph config
+         */
+        getGraphConfig: function () {
+            if (graphConfig) {
+                return Promise.resolve(graphConfig);
+            }
+            return handle("GET", "/v2/client-data/graph")
+                .then(result => {
+                    graphConfig = result;
+                    codeToObjectType = {};
+                    Object.keys(graphConfig).forEach(objectType => {
+                        if (graphConfig[objectType].code) {
+                            codeToObjectType[graphConfig[objectType].code] = objectType;
+                        }
+                    });
+                    return graphConfig;
+                });
+        },
+
+        /**
+         * Get edge config
+         */
+        getEdgeConfig: function (objOrID, edgeName) {
+            return this.getGraphConfig().then(config => {
+                let objectType;
+                if (typeof objOrID === "string") {
+                    if (objOrID in config) {
+                        objectType = objOrID;
+                    } else {
+                        objectType = codeToObjectType[objOrID.slice(-2)];
+                    }
+                } else {
+                    objectType = objOrID.object_type;
+                }
+                config = config[objectType];
+                if (!config) {
+                    throw Error(`Unknown object type '${objectType}'`);
+                }
+                if (!(edgeName in config.edges)) {
+                    throw Error(`Unknown edge for '${objectType}/${edgeName}'`);
+                }
+                return config.edges[edgeName];
+            });
+        },
+
+        /**
+         * Get object type by ID
+         */
+        getObjectType: function (id) {
+            return this.getGraphConfig().then(() => codeToObjectType[id.slice(-2)]);
+        },
+
+        /**
+         * Get object
+         */
+        getObject: function (id, options, author) {
+            return handle("GET", `/v2/client-data/graph/${id}`, "", author).then(result =>
+                options && options.expand ? this.expand(result, options.expand) : result
+            );
+        },
+
+        /**
+         * Get objects
+         */
+        getObjects: function (ids, options, author) {
+            if (typeof ids.slice(-1)[0] === "object") {
+                options = ids.slice(-1)[0];
+                ids = ids.slice(0, -1);
+            }
+            return handle("GET", `/v2/client-data/graph/${ids.join(",")}`, "", author).then(result =>
+                options && options.expand ? this.expand(result, options.expand) : result
+            );
+        },
+
+        /**
+         * Create object
+         */
+        createObject: (object, author, suppressEvent) => handle("POST", "/v2/client-data/graph", object, author, suppressEvent),
+
+        /**
+         * Update object
+         */
+        updateObject: (id, delta, author, suppressEvent) => handle("PATCH", `/v2/client-data/graph/${id}`, delta, author, suppressEvent),
+
+        /**
+         * Replace object
+         */
+        replaceObject: (id, object, author, suppressEvent) => handle("PUT", `/v2/client-data/graph/${id}`, object, author, suppressEvent),
+
+        /**
+         * Delete object
+         */
+        deleteObject: (id, author, suppressEvent) => handle("DELETE", `/v2/client-data/graph/${id}`, undefined, author, suppressEvent),
+
+        /**
+         * Get edge
+         */
+        getEdge: function (srcID, edgeName, dstID, options) {
+            return handle("GET", `/v2/client-data/graph/${srcID}/${edgeName}/${dstID}`).then(result =>
+                options && options.expand ? this.expand(result, options.expand) : result
+            );
+        },
+
+        /**
+         * Get edges
+         */
+        getEdges: function (srcID, edgeName, options) {
+            let url = `/v2/client-data/graph/${srcID}/${edgeName}`;
+            if (options) {
+                let params = [];
+                if (options.after !== undefined) {
+                    params.push(`after=${options.after}`);
+                }
+                if (options.count !== undefined) {
+                    params.push(`count=${options.count}`);
+                }
+                if (params.length) {
+                    url += `?${params.join("&")}`;
+                }
+            }
+            return handle("GET", url).then(result =>
+                options && options.expand ? this.expand(result, options.expand) : result
+            );
+        },
+
+        /**
+         * Create edge
+         */
+        createEdge: (srcID, edgeName, dstID, author, suppressEvent) => handle("POST", `/v2/client-data/graph/${srcID}/${edgeName}/${dstID}`, {}, author, suppressEvent),
+
+        /**
+         * Delete edge
+         */
+        deleteEdge: (srcID, edgeName, dstID, author, suppressEvent) => handle("DELETE", `/v2/client-data/graph/${srcID}/${edgeName}/${dstID}`, undefined, author, suppressEvent),
+
+        /**
+         * Create audit
+         */
+        createAudit: (auditLog, author) => handle("POST", "/v2/client-data/audit", auditLog, author),
+
+        /**
+         * Handle all pages
+         * @param {function(last) Promise<page>} query - execute query to server
+         * @param {function(page) Promise} handler - handle each page
+         */
+        forEachPage: (query, handler) => {
+            let count = 0;
+            let action = page =>
+                Promise.resolve().then(() => {
+                    return Promise.resolve().then(() => handler(page))
+                        .then(() => {
+                            count += page.results.length;
+                            if (page.last && count && count < page.count) {
+                                return query(page.last).then(action);
+                            }
+                        });
+                });
+            return query().then(action);
+        },
+
+        /**
+         * Expand object or page results with expand
+         */
+        expand: function (object, expand) {
+            // array of objects which need expand
+            let objects = [];
+            if (object.results || object.results === []) {
+                objects = object.results;
+            } else {
+                objects.push(object);
+            }
+            // "field or edge" -> "next level expand"
+            let expandMap = {};
+            let curly = 0; // count of curly brackets
+            let current = ""; // current expand item
+            expand.split("").forEach(ch => {
+                switch (ch) {
+                    case ",":
+                        if (curly === 0) {
+                            expandMap[current] = "";
+                            current = "";
+                        } else {
+                            current += ch;
+                        }
+                        break;
+                    case "{":
+                        current += ch;
+                        curly++;
+                        break;
+                    case "}":
+                        current += ch;
+                        curly--;
+                        break;
+                    default:
+                        current += ch;
+                }
+            });
+            if (current) {
+                expandMap[current] = "";
+            }
+            Object.keys(expandMap).forEach(key => {
+                let i = key.search("{");
+                if (i > 0) {
+                    delete expandMap[key];
+                    expandMap[key.slice(0, i)] = key.slice(i + 1, -1);
+                }
+            });
+
+            // apply expandMap to objects
+            if (objects && objects.length) {
+                objects = objects.map(obj => {
+                    return this.getGraphConfig().then(config => {
+                        let objCfg = config[obj.object_type]; // object config
+                        let promises = [];
+                        Object.keys(expandMap).forEach(field => {
+                            let options = {
+                                expand: expandMap[field]
+                            };
+                            let split = field.split("(");
+                            field = split[0];
+                            if (split[1]) {
+                                options.count = split[1].slice(0, -1);
+                            }
+                            let promise;
+                            if (field in obj && field in objCfg.fields) {
+                                if (!obj[field]) return;
+                                promise = this.getObject(obj[field], options);
+                            } else if (objCfg.edges && field in objCfg.edges) {
+                                promise = this.getEdges(obj.id, field, options);
+                            }
+                            if (promise) {
+                                promises.push(promise.then(res => {
+                                    obj[field] = res;
+                                }));
+                            }
+                        });
+                        return Promise.all(promises);
+                    });
+                });
+            }
+            return Promise.all(objects).then(() => object);
+        }
+    };
+}
+
+module.exports = newClient;
---
+++ searchV2/index.js
@@ -0,0 +1,251 @@
+"use strict";
+
+const elasticsearch = require("elasticsearch");
+
+let dateRegExp = /^\d{4}-\d{2}-\d{2}$/;
+
+/* eslint camelcase: 0 */
+
+class Searcher {
+    constructor(config) {
+        this.elastic = new elasticsearch.Client(config);
+    }
+    parseFilter(filters, rec) {
+        if (!(Object.keys(filters)).includes("AND") && !(Object.keys(filters)).includes("OR") && !rec) {
+            return this.parseFiltersFromService(filters);
+        }
+
+        const operatorLogic = (Object.keys(filters))[0];
+        let operator;
+        if (operatorLogic === "AND") operator = "must";
+        else if (operatorLogic === "OR") operator = "should";
+        const res = { bool: { [operator]: [] } };
+        filters[operatorLogic].forEach(item => {
+            const objectKeys = Object.keys(item);
+            objectKeys.forEach(key => {
+                if (["AND", "OR"].includes(key)) res.bool[operator].push(this.parseFilter(item, true));
+                else {
+                    if (item[key] === "!") res.bool[operator].push({ bool: { must_not: { exists: { field: key } } } })
+                    else if (item[key][0] === "!") res.bool[operator].push({ bool: { must_not: { term: { [key]: item[key].slice(1) } } } })
+                    else if (item[key].toLowerCase() === "exists") res.bool[operator].push({ bool: { must: { exists: { field: key } } } })
+                    else if (item[key] === "") res.bool[operator].push({ bool: { must: { exists: { field: key } } } })
+                    else res.bool[operator].push({ term: { [key]: item[key] } })
+                }
+
+            })
+        })
+        return res;
+    }
+    parseFiltersFromService(filters) {
+        const res = { bool: { must: [] } };
+        for (const field of Object.keys(filters)) {
+            if (filters[field] === "!") res.bool.must.push({ bool: { must_not: { exists: { field: field } } } });
+            else if (filters[field] === "") res.bool.must.push({ exists: { field: field } });
+            else res.bool.must.push({ term: { [field]: filters[field] } });
+        }
+        return res;
+    }
+    createRequest(options) {
+        let filters = [];
+        let sort_by_val;
+        let sort_by_field;
+        if (options.sort_by) {
+            sort_by_val = options.sort_by.split("(")[1].split(")")[0];
+            sort_by_field = options.sort_by.split("(")[0];
+        }
+
+        if (options.filters) {
+            filters = this.parseFilter(options.filters);
+        }
+
+        let range = {};
+        Object.keys(options.range || {}).forEach(key => {
+            range[key] = {};
+            if (options.range[key].gte) {
+                range[key].gte = options.range[key].gte;
+            }
+            if (options.range[key].lte) {
+                range[key].lte = options.range[key].lte;
+            }
+        });
+        const filterRange = [];
+        if (Object.keys(range).length) {
+            filterRange.push({ range: range });
+        }
+        let query;
+
+        if ((options.object === "laboratory_reqs" || options.object === "distributor_reqs") && options.group_by) {
+            query = {
+                query: {},
+                aggs: {}
+            };
+
+            query.aggs[options.object] = {
+                terms: {
+                    field: options.group_by,
+                    size: 5000
+                }
+            };
+            if (options.sort_by && sort_by_val && sort_by_field) {
+                if (sort_by_field === "result") {
+                    query.aggs[options.object].terms.order = { _count: sort_by_val }
+                }
+                if (sort_by_field === "object_name") {
+                    query.aggs[options.object].terms.order = { _key: sort_by_val }
+                }
+            }
+        } else if ((options.object === "laboratory_reqs" || options.object === "distributor_reqs") && options.func === "sum" && !options.group_by) {
+            query = {
+                query: {},
+                aggs: {}
+            };
+
+            query.aggs[options.object] = {
+                sum: {
+                    field: "tat"
+                }
+            };
+        } else if ((options.object === "laboratory_reqs" || options.object === "distributor_reqs") && options.func === "average" && !options.group_by) {
+            query = {
+                query: {},
+                aggs: {}
+            };
+
+            query.aggs[options.object] = {
+                avg: {
+                    field: "tat"
+                }
+            };
+        } else {
+            query = { query: {} };
+        }
+
+        let isEmail = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+
+        if (options.fields && options.q) {
+            let reg = /[^\w\s\.]/gi;
+            let checkNumberEmail = false;
+            if (isEmail.test(options.q)) {
+                let emailArr = options.q.split(".");
+                let lastSymbol = emailArr[emailArr.length - 2];
+                checkNumberEmail = Number.isInteger(parseInt(lastSymbol[lastSymbol.length - 1]));
+            }
+
+            if (checkNumberEmail) {
+                reg = /[^\w\s]/gi;
+            }
+            query.query.bool = query.query.bool || {};
+            options.q = options.q.replace("AND", "and");
+            options.q = options.q.replace(/  +/g, ' ');
+            options.q = options.q.split(" ").join(" AND ");
+            if (options.q.includes("/") || options.q.includes("(") || options.q.includes(")")) {
+                query.query.bool.must = {
+                    simple_query_string: {
+                        query: `*${options.q}*`,
+                        fields: options.fields
+                    }
+                };
+            } else {
+                query.query.bool.must = {
+                    query_string: {
+                        query: `*${options.q}*`,
+                        fields: options.fields
+                    }
+                };
+            }
+        }
+
+        if (options.count) {
+            query.size = options.count;
+        }
+
+        // Add parameter "from" instead of "search_after"
+        if (options.after) {
+            query.from = options.after;
+        }
+
+        // add sorting
+        query.sort = [];
+        if (options.sort) {
+            options.sort.forEach(sort => {
+                let sortField = sort.split("(")[0];
+                query.sort.push({ [sortField]: sort.toUpperCase().endsWith("(DESC)") ? "desc" : "asc" });
+            });
+        }
+        query.sort.push({ id: "asc" });
+        if (query.query.bool && query.query.bool.must) {
+            if (filters.bool.must) {
+                filters.bool.must.push(query.query.bool.must);
+                query.query.bool = filters.bool;
+            } else {
+                const query_string = query.query.bool.must;
+                query.query.bool.must = [query_string, { bool: filters.bool }];
+            }
+        } else {
+            query.query.bool = filters.bool;
+        }
+        if (query.query.bool) query.query.bool.filter = filterRange;
+        else {
+            query.query.bool = { filter: [] };
+        }
+        return query;
+    }
+
+    search(options) {
+        let query = Promise.resolve();
+        return query.then(searchAfter => {
+            let request = this.createRequest(options);
+            // Fix default sort by id for npi search
+            if (options.object === "log_line") {
+                request.sort.splice(request.sort.findIndex(el => el.hasOwnProperty('id')));
+                request.sort.push({ timestamp: "desc" });
+            }
+            if (options.object === "npi_entity") {
+                request.sort.splice(request.sort.findIndex(el => el.hasOwnProperty('id')));
+                request.sort.push({ npi_sort: "asc" });
+            }
+            if (options.object === "npi_location") {
+                request.sort.splice(request.sort.findIndex(el => el.hasOwnProperty('id')));
+                request.sort.push({ npi: "asc" });
+            }
+
+            return this.elastic.search({
+                index: options.object,
+                type: options.object,
+                body: request
+            });
+        }).then(body => {
+            // Fix for npi search
+            if (options.object === "laboratory_reqs" || options.object === "distributor_reqs") {
+                let res = {
+                    results: body.hits.hits.map(doc => doc._source),
+                    count: body.hits.total,
+                    aggregations: body.aggregations ? body.aggregations : ""
+
+                };
+                return res;
+            }
+            const returnObject = ["npi_location", "npi_entity", "log_line", "lab_test_position", "zip_code"];
+            if (returnObject.includes(options.object)) {
+                let res = {
+                    results: body.hits.hits.map(doc => doc._source),
+                    count: body.hits.total
+
+                };
+                return res;
+            }
+            // Regular search
+            let res = {
+                results: body.hits.hits.map(doc => doc._id),
+                count: body.hits.total
+            };
+            if (body.hits.hits.length > 0) {
+                res.first = body.hits.hits[0]._source.id;
+                res.last = body.hits.hits.slice(-1)[0]._source.id;
+            }
+            return res;
+        });
+    }
+}
+
+module.exports.Searcher = Searcher;
