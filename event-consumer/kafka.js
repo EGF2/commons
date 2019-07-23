bf7a1c445e020098c3ddd1dcf4d68db9f541425d
win-dox <schamraev.d@yandex.ru>
2019-07-23 16:30:39 +0300

    rewrite client kafka

--- event-consumer/kafka.js
+++ event-consumer/kafka.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const kafka = require("no-kafka");
 
 /**
@@ -7,47 +5,39 @@ const kafka = require("no-kafka");
  * @param eventHandler - event handler
  * @param errorHandler - error handler
  */
-function newConsumer(config, eventHandler, errorHandler) {
-    let consumer = new kafka.SimpleConsumer({
+
+const handler = (eventHandler, errorHandler, consumer) => async (messageSet, topic, partition) => {
+    try {
+        for (const message of messageSet) {
+            await eventHandler(JSON.parse(message.message.value.toString("utf8")))
+            await consumer.commitOffset({
+                topic,
+                partition,
+                offset: message.offset
+            });
+        }
+    } catch (e) {
+        errorHandler(e, consumer);
+    }
+};
+
+const newConsumer = async (config, eventHandler, errorHandler) => {
+    const consumer = new kafka.SimpleConsumer({
         connectionString: config.kafka.hosts.join(","),
         groupId: config["consumer-group"],
         clientId: config.kafka["client-id"]
     });
-    return consumer.init().then(() => {
-        return consumer.fetchOffset([{
-            topic: config.kafka.topic,
-            partition: 0
-        }])
-        .then(fetchOffset => {
-            let offset = fetchOffset[0].offset;
-            if (offset > -1) {
-                offset++;
-            }
-            consumer.subscribe(config.kafka.topic, 0, {offset}, (messageSet, topic, partition) => {
-                return Promise.all(messageSet.map(message =>
-                    eventHandler(JSON.parse(message.message.value.toString("utf8")))
-                ))
-                .then(() => {
-                    let last = messageSet[messageSet.length - 1];
-                    return consumer.commitOffset({
-                        topic,
-                        partition,
-                        offset: last.offset
-                    });
-                })
-                .catch(err => {
-                    errorHandler(err);
-                });
-            });
-        })
-        .catch(err => {
-            errorHandler(err);
-            process.exit(101);
-        });
-    })
-    .catch(err => {
-        errorHandler(err);
-        throw err;
-    });
-}
+
+    await consumer.init();
+    const fetchOffset = await consumer.fetchOffset([{
+        topic: config.kafka.topic,
+        partition: 0
+    }]);
+    let offset = fetchOffset[0].offset;
+    if (offset > -1) {
+        offset++;
+    }
+    consumer.subscribe(config.kafka.topic, 0, { offset }, handler(eventHandler, errorHandler, consumer));
+};
+
 module.exports = newConsumer;
