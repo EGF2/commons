1dbbc3429e1aaa637f7a23f6d20175bf58804f3b
win-dox <schamraev.d@yandex.ru>
2019-10-07 11:22:40 +0300

    Change kafka module and remove v1

--- event-consumerV2/kafka.js
+++ event-consumerV2/kafka.js
@@ -1,49 +1,58 @@
-const kafka = require("no-kafka");
-
+const kafka = require("kafka-node");
+const Transform = require("stream").Transform;
 /**
  * @param config - kafka config
  * @param eventHandler - event handler
  * @param errorHandler - error handler
  */
 
-const handler = (eventHandler, errorHandler, consumer) => async (messageSet, topic, partition) => {
-    try {
-        for (const message of messageSet) {
-            await eventHandler(JSON.parse(message.message.value.toString("utf8")))
-            await consumer.commitOffset({
+const newConsumer = async (config, eventHandler, errorHandler, Logger) => {
+    const Log = new Logger(__filename);
+    const topic = config.kafka.topicV2;
+    const options = {
+        kafkaHost: config.kafka.hosts.join(","),
+        groupId: config.groupId,
+        protocol: ["roundrobin"],
+        encoding: "utf8",
+        onRebalance: (isAlreadyMember, callback) => {
+            callback();
+        },
+        fromOffset: "latest",
+        outOfRangeOffset: "earliest",
+        autoCommit: false
+    };
+    const consumerGroup = new kafka.ConsumerGroupStream(
+    options,
+    topic
+  );
+    const resultProducer = new kafka.ProducerStream();
+    const messageTransform = new Transform({
+        objectMode: true,
+        decodeStrings: true,
+        async transform(message, encoding, callback) {
+            try {
+                await eventHandler(JSON.parce(message.value));
+            } catch (e) {
+                errorHandler(e);
+            }
+            consumerGroup.commit(message, true, () => {});
+            callback(null, {
                 topic,
-                partition,
-                offset: message.offset
+                messages: message
             });
         }
-    } catch (e) {
-        errorHandler(e, consumer);
-    }
-};
-
-const newConsumer = async (config, eventHandler, errorHandler) => {
-    const consumer = new kafka.SimpleConsumer({
-        connectionString: config.kafka.hosts.join(","),
-        groupId: `${config["consumer-group"]}V2`,
-        clientId: config.kafka["client-id"],
-        logger: {
-            logFunction: (q, w, e, error, t, y, u, i, o = "", p = "") => {
-                console.log(q, w, e, error, t, y, u, i, o, p);
-                if (error.includes("NoKafkaConnectionError")) errorHandler();
-              }
-          }
     });
 
-    await consumer.init();
-    const fetchOffset = await consumer.fetchOffset([{
-        topic: config.kafka.topicV2,
-        partition: 0
-    }]);
-    let offset = fetchOffset[0].offset;
-    if (offset > -1) {
-        offset++;
-    }
-    consumer.subscribe(config.kafka.topicV2, 0, { offset }, handler(eventHandler, errorHandler, consumer));
+    consumerGroup.on("error", e => {
+        Log.error("Error kafka", e);
+        errorHandler(e);
+    });
+
+    consumerGroup.on("connect", () => {
+        Log.info("Kafka connect", {host: options.kafkaHost, groupId: options.groupId, topic});
+    });
+
+    consumerGroup.pipe(messageTransform).pipe(resultProducer);
 };
 
 module.exports = newConsumer;
