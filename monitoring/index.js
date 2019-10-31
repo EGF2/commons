686c83fe04700caec63bb9a3c477d86fda13d104
win-dox <schamraev.d@yandex.ru>
2019-10-31 16:21:52 +0300

    Test

--- monitoring/index.js
+++ monitoring/index.js
@@ -18,14 +18,26 @@ const getIPAddress = () => {
   return "0.0.0.0";
 };
 
-const pingMonitoring = (url, serviceName, status) => {
-  return axios({
-    method: "POST",
-    url,
-    data: {
-      service_type: serviceName,
-      service_ip: getIPAddress(),
-      status,
+const pingMonitoring = (url, serviceName, status, port) => {
+  return new Promise(async (resolve, reject) => {
+    const resonce = await axios.get({
+      method: "GET",
+      url: `http://localhost:${process.env.egf_port || port}/v2/metadata`
+    });
+    console.log(resonce);
+    try {
+      const res = await axios({
+        method: "POST",
+        url,
+        data: {
+          service_type: serviceName,
+          service_ip: getIPAddress(),
+          status,
+        }
+      });
+      resolve(res)
+    } catch (e) {
+      reject(e)
     }
   });
 };
