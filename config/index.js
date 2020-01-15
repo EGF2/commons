c0bdd6adf986323476805d6e64092b600d9ae2ac
win-dox <schamraev.d@yandex.ru>
2020-01-15 10:28:18 +0300

    Add merge config

--- config/index.js
+++ config/index.js
@@ -1,21 +1,40 @@
+const _merge = require("lodash/merge");
+
 let config = {};
 
+/*
+    This module reads variables from env with the prefix "egf_" and generates a config
+    that will return as the result of the function.
+    The project root directory is expected to contain the config/config.js file
+    In the config.js file the shouldVar(Array) field is expected, which indicates which variables are expected
+    if there are no variables specified in env, an exception will be thrown in shouldVar.
+    All other fields in config.js will be merged with the config from env, priority for env.
+*/
+
 const initConfig = () => {
-    console.log(__dirname)
     if (Object.keys(config).length) return config;
-    let configApp = require("./config/config.js");
+    let configApp;
+    try {
+        configApp = require("./config/config.js");
+    } catch (e) {
+        configApp = {};
+        console.log("WARNING. Not found file config/config.js")
+    }
     if (!process.env) throw new Error("Not found var in env");
 
-    for(const field of configApp.shouldVar) {
-        if (process.env[`egf_${field}`]) config[field] = process.env[`egf_${field}`];
-    }
-    delete configApp.shouldVar;
+    if(configApp.shouldVar) {
+        for(const field of configApp.shouldVar) {
+            if (process.env[`egf_${field}`]) config[field] = process.env[`egf_${field}`];
+        }
+        if(Object.keys(config).length !== configApp.shouldVar.length)
+            throw new Error("Not all variables specified in shouldVar are found in env")
 
-    if (Object.keys(configApp).length) {
-        config = Object.assign(configApp, config);
-    }
+        delete configApp.shouldVar;
+    } else console.log("WARNING. Variable 'shouldVar' in config is empty")
+
+
+    if (Object.keys(configApp).length) config = _merge(configApp, config);
     return config;
-}
-
+};
 
 module.exports = initConfig();
